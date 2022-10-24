42e46857fbe75f93d2d2282acdc4ba22960ba51f
MELTEM <meltemfsanaldi@gmail.com>
2022-10-24 13:28:36 +0300

    sec

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -3,52 +3,46 @@ import "../index.css"
 
 const Contact = () => {
   return (
-    <div className='cards'>
+    <div className="cards">
       <div className="container">
-        <h1 className="text-center mt-4">CONTACT FORM</h1>
+        <h1>CONTACT FORM</h1>
         <form>
           <div className="mb-3">
-            <label htmlFor="username" className="form-label">
-              Username
-            </label><br />
+            <label htmlFor="username">Username</label>
+            <br />
             <input
               name="username"
               type="text"
               className="form-control"
               id="username"
               placeholder="Enter your username"
-             
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="email" className="form-label">
-              Email
-            </label><br />
+            <label htmlFor="email">Email</label>
+            <br />
             <input
               name="email"
               type="email"
               className="form-control"
               id="email"
               placeholder="Enter your email"
-            
             />
           </div>
 
           <div className="mb-3">
-            <label htmlFor="password" className="form-label">
-              Password
-            </label><br />
+            <label htmlFor="password">Password</label>
+            <br />
             <input
               name="password"
               type="password"
               className="form-control"
               id="password"
               placeholder="Enter your password"
-           
             />
           </div>
-          <div className="text-center">
-            <button className="btn btn-danger mb-4">Submit</button>
+          <div className="btn">
+            <button>Submit</button>
           </div>
         </form>
       </div>
